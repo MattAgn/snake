42d162734c80be79a510153015917941f074f24f
mattInnov <mat.alingrin@gmail.com>
2018-05-22 19:35:16 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Canvas from './Canvas';
+
+class App extends Component {
+
+  render() {
+    return (
+      <Canvas/>
+    );
+  }
+}
+
+export default App;
